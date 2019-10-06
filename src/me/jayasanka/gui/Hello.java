3265c769be0fec14524e9d63132ed691d3b28320
gravewalker666 <heshan.andris.pro@gmail.com>
2019-10-06 10:50:09 +0530

    add Hello.java

---
+++ src/me/jayasanka/gui/Hello.java
@@ -0,0 +1,15 @@
+package me.jayasanka.gui;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Hello extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Hello kohomada koheda inne");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
